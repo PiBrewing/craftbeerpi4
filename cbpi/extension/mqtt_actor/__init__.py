c76a878f6072e0ca0a39447fec6931d00730aa17
JamFfm <Jan.Battermann@t-online.de>
2021-12-03 11:39:17 +0100

    Added MQTTActor1 with support for Tasmota Sonoff

--- cbpi/extension/mqtt_actor/__init__.py
+++ cbpi/extension/mqtt_actor/__init__.py
@@ -1,33 +1,71 @@
 # -*- coding: utf-8 -*-
-
-# Topic pattern:
-# Tasmota on sonoff:
-# tasmota on sonoff TH16 or TH10 topic: cmnd/<tasmota_name_of _device>/Power
-# or like eg. cmnd/tasmota_F9713B/Power
-# Power in the meaning of on/off. Power as % is not supported
-# Have a look at https://stevessmarthomeguide.com/setting-up-the-sonoff-tasmota-mqtt-switch/
-# In Tasmota adjust configuration-> configure logging->Telemetry period (300) from 300s to 10s
-# to speed up the sensor (I know this is the actor section)
-# If you need to change payload please edit in function async def on(..) and async def off..)
-#
-# EspEasy
-# use this link:
-# https://nerdiy.de/howto-espeasy-mqtt-server-konfigurieren-und-topics-abonnieren/
-# You may use translate function of the browser to translate the language
-# If you need to change payload please edit in function async def on(..) and async def off..)
-
 import asyncio
-import logging
 import json
+from cbpi.api import parameters, Property, CBPiActor
 from cbpi.api import *
+import logging
 
 logger = logging.getLogger(__name__)
+
+
+@parameters([Property.Text(label="Topic", configurable=True, description = "MQTT Topic")])
+class MQTTActor(CBPiActor):
+
+    # Custom property which can be configured by the user
+    @action("Set Power", parameters=[Property.Number(label="Power", configurable=True,description="Power Setting [0-100]")])
+    async def setpower(self,Power = 100 ,**kwargs):
+        self.power=round(Power)
+        if self.power < 0:
+            self.power = 0
+        if self.power > 100:
+            self.power = 100           
+        await self.set_power(self.power)      
+
+    def __init__(self, cbpi, id, props):
+        super(MQTTActor, self).__init__(cbpi, id, props)
+
+    async def on_start(self):
+         self.topic = self.props.get("Topic", None)
+         self.power = 100
+
+    async def on(self, power=None):
+        if power is not None:
+            if power != self.power:
+                power = min(100, power)
+                power = max(0, power)
+                self.power = round(power)
+        await self.cbpi.satellite.publish(self.topic, json.dumps(
+            {"state": "on", "power": self.power}), True)
+        self.state = True
+        pass
+
+    async def off(self):
+        self.state = False
+        await self.cbpi.satellite.publish(self.topic, json.dumps(
+            {"state": "off", "power": self.power}), True)
+        pass
+
+    async def run(self):
+        while self.running:
+            await asyncio.sleep(1)
+
+    def get_state(self):
+        return self.state
+
+    async def set_power(self, power):
+        self.power = round(power)
+        if self.state == True:
+            await self.on(power)
+        else:
+            await self.off()
+        await self.cbpi.actor.actor_update(self.id,power)
+        pass
 
 
 @parameters([Property.Text(label="Topic", configurable=True, description="MQTT Topic"),
              Property.Select(label="select_Mqtt_System", options=["default", "tasmota on sonoff THx", "ESPEasy"],
                              description="different Mqtt Systems listen to different payloads")])
-class MQTTActor(CBPiActor):
+class MQTTActor1(CBPiActor):
 
     @action("Set Power",
             parameters=[Property.Number(label="Power", configurable=True, description="Power Setting [0-100]")])
@@ -40,7 +78,7 @@ class MQTTActor(CBPiActor):
         await self.set_power(self.power)
 
     def __init__(self, cbpi, id, props):
-        super(MQTTActor, self).__init__(cbpi, id, props)
+        super(MQTTActor1, self).__init__(cbpi, id, props)
 
     async def on_start(self):
         self.topic = self.props.get("Topic", None)
@@ -113,3 +151,4 @@ def setup(cbpi):
     '''
     if str(cbpi.static_config.get("mqtt", False)).lower() == "true":
         cbpi.plugin.register("MQTTActor", MQTTActor)
+        cbpi.plugin.register("MQTTActor1", MQTTActor1)
